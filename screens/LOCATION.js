3bdce12be67a3a8ae640150dee683718bd3c24cc
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-03-22 23:55:43 +0500

    Implemented Maps Markers

--- screens/LOCATION.js
+++ screens/LOCATION.js
@@ -175,3 +175,82 @@
 
 
 // export default LOCATION;
+
+
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
+export default function LOCATION() {
+
+    const [Marker, setMarker] = React.useState([
+        {
+            id: 1,
+            latitude: 37.78825,
+            longitude: -122.4324,
+            title: 'You are here',
+            description: 'Your current location',
+        },
+        {
+            id: 2,
+            latitude: 67.78825,
+            longitude: -22.4324,
+            title: 'You are here',
+            description: 'Your current location',
+        },
+        {
+            id: 3,
+            latitude: 87.78825,
+            longitude: -122.4324,
+            title: 'You are here',
+            description: 'Your current location',
+        },
+
+
+    ]);
+    return (
+
+
+
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                style={styles.map}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+
+            >
+                {
+                    Marker.map((maker) => {
+                        return (
+                            <Marker
+                                key={maker.id}
+                                coordinate={{ latitude: maker.latitude, longitude: maker.longitude }}
+                                title={maker.title}
+                                description={maker.description}
+                            />
+                        );
+                        })
+                }
+            </MapView>
+        </View>
+
+
+    )
+}
